e9711535f150073ac084975a1f0368035c1f2fbb
shicy <shicy85@163.com>
2017-08-07 21:02:56 +0800

    顶部拦设计

--- framework/HeaderView.js
+++ framework/HeaderView.js
@@ -13,9 +13,13 @@ var HeaderView = VRender.UIView.extend(module, {
 	renderView: function () {
 		HeaderView.__super__.renderView.call(this);
 
-		var content = VRender.$("<div class='content'></div>").appendTo(this.$el);
+		var target = VRender.$("<div class='container'></div>").appendTo(this.$el);
 
-		var menus = VRender.$("<ul class='menus'></ul>").appendTo(content);
+		var content = VRender.$("<div class='content'></div>").appendTo(target);
+		content.append("<a class='logo' href='/'></a>")
+
+		var menus = VRender.$("<div class='topmenu' tabindex='-1'></div>").appendTo(content);
+		menus = VRender.$("<ul class='menus'></ul>").appendTo(menus);
 		menus.append("<li class='menu quickstart'><a href='/quickstart'>快速入门</a></li>");
 		menus.append("<li class='menu documents'><a href='/documents'>说明文档</a></li>");
 		menus.append("<li class='menu components'><a href='/components'>组件</a></li>");
