2fd016aab28ed4635b8f5ddd3acba1dadc963502
shicy <shicy85@163.com>
2017-08-07 21:12:50 +0800

    快速入门页面设计

--- pages/quickstart/index.front.js
+++ pages/quickstart/index.front.js
@@ -4,6 +4,34 @@
  * Created on 2017-07-07
  */
 
-define(function ($, VR) {
-	console.log("shicy", "11111111111111111111");
-});
+var body = $("body");
+
+// 菜单
+var menus = body.find("aside li");
+
+// 章节锚点
+var sections = body.find("article h1 a");
+
+
+if (body.attr("device") == "pc") {
+	setTimeout(function () {
+		var positions = Utils.map(body.find("article h1 a"), function (item) {
+			return item.offset().top;
+		});
+
+		$(window).on("scroll", function (e) {
+			menus.removeClass("active");
+			var scrollTop = body.scrollTop() + 6;
+			for (var i = positions.length - 1; i >= 0; i--) {
+				if (scrollTop > positions[i]) {
+					menus.eq(i).addClass("active");
+					break;
+				}
+			}
+		}).scroll();
+	}, 0);
+}
+
+
+
+
