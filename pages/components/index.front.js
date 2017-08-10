cfdfc103c7926696c46b6ea42d1a277a6e1be536
shicy <shicy85@163.com>
2017-08-10 22:53:35 +0800

    组件

---
+++ pages/components/index.front.js
@@ -0,0 +1,7 @@
+/**
+ * 组件
+ * @author shicy <shicy85@163.com>
+ * Created on 2017-07-09
+ */
+
+ 
--- pages/components/index.js
+++ pages/components/index.js
@@ -6,6 +6,7 @@
 
 var VRender = require(__vrender);
 var HeaderView = require(__basedir + "/framework/HeaderView");
+var SideMenuView = require("./SideMenuView");
 
 
 var ComonentsView = VRender.PageView.extend(module, {
@@ -13,11 +14,23 @@ var ComonentsView = VRender.PageView.extend(module, {
 		return "VRender-组件";
 	},
 
+	// doInit: function () {
+	// 	ComonentsView.__super__.doInit.call(this);
+	// },
+
 	renderBody: function (body) {
 		ComonentsView.__super__.renderBody.call(this, body);
 
 		new HeaderView(this, {active: "components"}).render(body);
+
+		var mainBody = VRender.$("<div id='main-body'></div>").appendTo(body);
+
+		new SideMenuView().render(mainBody);
+
+		mainBody.write("<div id='singlepage-container' class='main-container'></div>");
 	}
 });
 
-ComonentsView.import(["/css/style.css"]);
+ComonentsView.use(VRender.plugins.SinglePage);
+
+ComonentsView.import("/css/style.css");
