90ab4c083a1254bdcc8592e92117dc7de3150c3b
shicy <shicy85@163.com>
2017-12-20 18:07:22 +0800

    组件 tabbar 说明文档修改

--- pages/components/modules/tabbar.js
+++ pages/components/modules/tabbar.js
@@ -28,6 +28,8 @@ var TabbarModule = BaseModule.extend(module, {
 
 	renderExamples: function () {
 		this.showExample1();
+		this.showExample2();
+		this.showExample3();
 	},
 
 	showExample1: function () {
@@ -45,6 +47,41 @@ var TabbarModule = BaseModule.extend(module, {
 		source.push("new UITabbar(context, {data: tabs, selectedIndex: 0}).render(target)");
 		source.push("// 浏览器端创建");
 		source.push("UITabbar.create({target: [elem], data: tabs, selectedIndex: 0});");
+
+		this.showDemo(example, demo, source, true);
+	},
+
+	showExample2: function () {
+		var example = this.addExample("可关闭标签");
+
+		var tabs = [{label: "标签1", name: "tab1"}, {label: "标签2", name: "tab2", closable: true}, 
+			{label: "标签3", name: "tab3", closable: true}, {label: "标签4", name: "tab4", closable: true}];
+		var demo = new UITabbar(this, {data: tabs, selectedIndex: 1});
+
+		var source = [];
+		source.push("var tabs = [];");
+		source.push("tabs.push({label: '标签1', name: 'tab1'});");
+		source.push("tabs.push({label: '标签2', name: 'tab2', closable: true});");
+		source.push("tabs.push({label: '标签3', name: 'tab3', closable: true});");
+		source.push("tabs.push({label: '标签4', name: 'tab4', closable: true});");
+		source.push("new UITabbar(context, {data: tabs, selectedIndex: 1}).render(target)");
+
+		this.showDemo(example, demo, source);
+	},
+
+	showExample3: function () {
+		var example = this.addExample("更多标签超出显示");
+
+		var tabs = [];
+		for (var i = 1; i <= 20; i++) {
+			tabs.push({label: "标签" + i, name: "tab" + i});
+		}
+		var demo = new UITabbar(this, {data: tabs, selectedIndex: 15});
+
+		var source = [];
+		source.push("var tabs = [];");
+		source.push("for (var i = 1; i <= 20; i++) {\n\ttabs.push({label, '标签' + i, name: 'tab' + i});\n}");
+		source.push("new UITabbar(context, {data: tabs, selectedIndex: 15}).render(target);");
 
 		this.showDemo(example, demo, source);
 	}
