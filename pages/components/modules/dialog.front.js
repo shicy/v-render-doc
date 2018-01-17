2fc7f1d3659754369552a8b90627574de6548892
shicy <shicy85@163.com>
2018-01-17 16:27:05 +0800

    组件 dialog 文档修改

--- pages/components/modules/dialog.front.js
+++ pages/components/modules/dialog.front.js
@@ -8,9 +8,9 @@ var view = $(".comp-dialog"); console.log(view);
 
 
 view.on("click", "[name='example1-btn']", function (e) {
-	var dialog = UIDialog.create({title: "对话框标题", content: "对话框内容<br/><a>支持HTML格式</a>"});
-	dialog.on("btnclick", function (e, name) {
-		alert("你选择了“" + (name == "submit" ? "确定" : "取消") + "”");
-		dialog.close();
-	});
+	// var dialog = UIDialog.create({title: "对话框标题", content: "对话框内容<br/><a>支持HTML格式</a>"});
+	// dialog.on("btnclick", function (e, name) {
+	// 	alert("你选择了“" + (name == "submit" ? "确定" : "取消") + "”");
+	// 	dialog.close();
+	// });
 });
--- pages/components/modules/dialog.js
+++ pages/components/modules/dialog.js
@@ -9,7 +9,9 @@ var BaseModule = require("./_base");
 
 
 var UIGroup = VRender.UIGroup;
+var UIHGroup = VRender.UIHGroup;
 var UIButton = VRender.UIButton;
+var UITextView = VRender.UITextView;
 var UIDialog = VRender.UIDialog;
 
 var DialogModule = BaseModule.extend(module, {
@@ -29,12 +31,17 @@ var DialogModule = BaseModule.extend(module, {
 
 	renderExamples: function () {
 		this.showExample1();
+		this.showExample2();
+		this.showExample3();
+		this.showExample4();
+		this.showExample5();
+		// this.showExample6();
 	},
 
 	showExample1: function () {
 		var example = this.addExample("基本用法");
 
-		var demo = new UIGroup(this, {gap: 10});
+		var demo = new UIHGroup(this, {gap: 10});
 		demo.append(new UIButton(this, {name: "example1-btn", label: "点击打开对话框"}));
 
 		var source = [];
@@ -45,10 +52,131 @@ var DialogModule = BaseModule.extend(module, {
 		source.push("    title: '对话框标题',");
 		source.push("    content: '对话框内容<br/><a>支持HTML格式</a>'");
 		source.push("});");
-		source.push("dialog.on('btnclick', function (e, name) {");
-		source.push("    alert('你选择了“' + (name == 'submit' ? '确定' : '取消') + '”');");
-		source.push("    dialog.close();");
+		// source.push("dialog.on('btnclk', function (e, name) {");
+		// source.push("    alert('你选择了“' + (name == 'submit' ? '确定' : '取消') + '”');");
+		// source.push("    dialog.close();");
+		// source.push("});");
+
+		this.showDemo(example, demo, source, true);
+	},
+
+	showExample2: function () {
+		var example = this.addExample("对话框大小");
+
+		var demo = new UIHGroup(this, {gap: 10});
+		demo.append(new UIButton(this, {name: "example2-small", label: "小对话框"}));
+		demo.append(new UIButton(this, {name: "example2-normal", label: "中等对话框"}));
+		demo.append(new UIButton(this, {name: "example2-big", label: "大对话框"}));
+		demo.append(new UIButton(this, {name: "example2-auto", label: "自适应大小"}));
+
+		var source = [];
+		source.push("new UIDialog(context, {size: 'small'});");
+		source.push("new UIDialog(context, {size: 'normal'});");
+		source.push("new UIDialog(context, {size: 'big'});");
+		source.push("var contentView = \"<div style='width: 450px; height: 320px;'>宽450, 高320</div>\";");
+		source.push("new UIDialog(context, {size: 'auto', content: contentView});");
+
+		this.showDemo(example, demo, source);
+	},
+
+	showExample3: function () {
+		var example = this.addExample("自定义按钮");
+
+		var demo = new UIHGroup(this, {gap: 10});
+		demo.append(new UIButton(this, {name: "example3-btn", label: "点击打开对话框"}));
+
+		var source = [];
+		source.push("var buttons = [];");
+		source.push("buttons.push({name: 'cancel', label: '取消', type: 'cancel'});");
+		source.push("buttons.push({name: 'reset', label: '重置', type: 'save'});");
+		source.push("buttons.push({name: 'ok', label: '保存', type: 'primary', waitting: true});");
+		source.push("buttons.push({name: 'wait', label: '等待5秒关闭', waitting: 5000});");
+
+		source.push("var contentView = new UIGroup(context);")
+		source.push("var dialog = new UIDialog(context, {buttons: buttons, content: contentView});");
+
+		source.push("dialog.on('btnOk', function () {");
+		source.push("    contentView.append('<div>点击了“保存”按钮..</div>');");
+		source.push("    setTimeout(function () {");
+		source.push("        dialog.setButtonWaitting('ok', false);");
+		source.push("    }, 2000);");
 		source.push("});");
+
+		source.push("dialog.on('btnCancel', function () {");
+		source.push("    contentView.append('<div>点击了“取消”按钮..</div>');");
+		source.push("});");
+
+		source.push("dialog.on('btnReset', function () {");
+		source.push("    contentView.append('<div>点击了“重置”按钮..</div>');");
+		source.push("});");
+
+		source.push("dialog.on('btnWait', function () {");
+		source.push("    contentView.append('<div>5秒后关闭对话框</div>');");
+		source.push("    var seconds = 5;");
+		source.push("    var timerId = setInterval(function () {");
+		source.push("        if (--seconds <= 0) {");
+		source.push("            clearInterval(timerId);");
+		source.push("            dialog.close();");
+		source.push("        }");
+		source.push("        contentView.append('<div>' + seconds + '秒后关闭对话框</div>');");
+		source.push("    }, 1000};");
+		source.push("});");
+
+		source.push("dialog.on('btnclk', function (e, name) {});");
+		source.push("    contentView.append('<div>统一事件“btnclk”，按钮名称：' + name + '</div>');");
+		source.push("});");
+
+		this.showDemo(example, demo, source);
+	},
+
+	showExample4: function () {
+		var example = this.addExample("自定义内边距");
+
+		var demo = new UIHGroup(this, {gap: 10});
+		demo.append(new UIButton(this, {name: "example4-btn", label: "无内边距对话框"}));
+
+		var source = [];
+		source.push("new UIDialog(context, {padding: 0});");
+
+		this.showDemo(example, demo, source);
+	},
+
+	showExample5: function () {
+		var example = this.addExample("设置组件内容");
+
+		var demo = new UIGroup(this);
+		demo.add(new UIHGroup(this, {gap: 10}))
+			.append(new UIButton(this, {name: "example5-btn", label: "点击打开对话框"}));
+
+		var contentView = new UIGroup(this);
+		contentView.append("<div>添加一个组件作为对话框内容</div>");
+		contentView.add(new UIGroup(this)).append(new UITextView(this, {prompt: "输入框"}));
+		contentView.add(new UIGroup(this)).append(new UIButton(this, {label: "按钮"}));
+		demo.add(new UIGroup(this))
+			.append(new UIDialog(this, {content: contentView, showbtn: "[name='example5-btn']"}));
+
+		var source = [];
+		source.push("var contentView = new UIGroup(context);");
+		source.push("contentView.append('<div>添加一个组件作为对话框内容</div>')");
+		source.push("contentView.add(new UIGroup(context)).append(new UITextView(context, {prompt: '输入框'}));");
+		source.push("contentView.add(new UIGroup(context)).append(new UIButton(context, {label: '按钮'}));");
+		source.push("new UIDialog(context, {content: contentView});");
+
+		this.showDemo(example, demo, source);
+	},
+
+	showExample6: function () {
+		var example = this.addExample("异步加载内容");
+
+		var demo = new UIGroup(this);
+		demo.add(new UIHGroup(this, {gap: 10}))
+			.append(new UIButton(this, {name: "example6-btn", label: "点击打开对话框"}));
+
+		demo.add(new UIGroup(this))
+			.append(new UIDialog(this, {module: "/test/module", showbtn: "[name='example6-btn']"}));
+
+		var source = [];
+		source.push("new UIDialog(context, {module: '/test/module'});");
 
 		this.showDemo(example, demo, source);
 	}
