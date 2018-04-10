5cddfa75cb1664351f8da4e1745857ccaacf5d66
shicy <shicy85@163.com>
2018-04-10 23:22:38 +0800

    组件 panel 文档修改

--- pages/components/modules/panel.js
+++ pages/components/modules/panel.js
@@ -38,9 +38,12 @@ var PanelModule = BaseModule.extend(module, {
 		var demo = new UIPanel(this, {title: "标题", content: "内容<br/><a>支持富文本</a>"});
 
 		var source = [];
-		source.push("new UIPanel(context, {title: '标题', context: '内容<br/>a>支持富文本</a>'}).render(target)");
+		source.push("new UIPanel(context, {");
+		source.push("    title: '标题',");
+		source.push("    context: '内容<br/>a>支持富文本</a>'");
+		source.push("});");
 
-		this.showDemo(example, demo, source);
+		this.showDemo(example, demo, source, true);
 	},
 
 	showExample2: function () {
@@ -48,17 +51,49 @@ var PanelModule = BaseModule.extend(module, {
 
 		var buttons = [];
 		buttons.push({name: "btn1", label: "默认按钮"});
-		buttons.push({name: "btn2", label: "下拉按钮", items: [{name: "btn21", label: "下拉按钮1"}, 
-			{name: "btn22", label: "下拉按钮2"}, 
-			{name: "btn23", label: "下拉按钮3"}, 
-			{name: "btn24", label: "下拉按钮4"}]});
-		buttons.push({name: "btn3", icon: "/image/icons/d01.png", tooltip: "图标按钮"});
+		buttons.push({name: "btn2", label: "下拉按钮", toggle: true,
+			items: [{name: "btn21", label: "下拉按钮1"}, {name: "btn22", label: "下拉按钮2"}, 
+				{name: "btn23", label: "下拉按钮3"}, {name: "btn24", label: "下拉按钮4"}]});
+		buttons.push({name: "btn3", label: "带图标按钮", icon: "/image/icons/b04.png"})
+		buttons.push({name: "btn4", icon: "/image/icons/b05.png", tooltip: "图标按钮", toggle: true});
+		buttons.push({name: "btn5", label: false, icon: "/image/icons/d01.png", 
+			items: [{name: "btn51", label: "选项1", icon: "/image/icons/b02.png"},
+				{name: "btn52", label: "选项2", icon: "/image/icons/b03.png"},
+				{name: "btn53", label: "选项3", icon: "/image/icons/b04.png"}]});
 
-
-		var demo = new UIPanel(this, {title: "", buttons: buttons});
+		var demo = new UIPanel(this, {title: "自定义按钮", buttons: buttons});
 
 		var source = [];
-
+		source.push("new UIPanel(content, {");
+		source.push("    title: '自定义按钮',");
+		source.push("    buttons: [{");
+		source.push("        name: 'btn1', label: '默认按钮'");
+		source.push("    }, {");
+		source.push("        name: 'btn2', label: '下拉按钮', toggle: true,");
+		source.push("        items: [{");
+		source.push("            name: 'btn21', label: '下拉按钮1'");
+		source.push("        }, {");
+		source.push("            name: 'btn22', label: '下拉按钮2'");
+		source.push("        }, {");
+		source.push("            name: 'btn23', label: '下拉按钮3'");
+		source.push("        }, {");
+		source.push("            name: 'btn24', label: '下拉按钮4'");
+		source.push("        }]");
+		source.push("    }, {");
+		source.push("        name: 'btn3', label: '带图标按钮', icon: '/image/icons/b04.png'");
+		source.push("    }, {");
+		source.push("        name: 'btn4', icon: '/image/icons/b05.png', tooltip: '图标按钮', toggle: true");
+		source.push("    }, {");
+		source.push("        name: 'btn5', label: false, icon: '/image/icons/d01.png',");
+		source.push("        items: [{");
+		source.push("            name: 'btn51', label: '选项1', icon: '/image/icons/b02.png'");
+		source.push("        }, {");
+		source.push("            name: 'btn52', label: '选项2', icon: '/image/icons/b03.png'");
+		source.push("        }, {");
+		source.push("            name: 'btn53', label: '选项3', icon: '/image/icons/b04.png'");
+		source.push("        }]");
+		source.push("    }]");
+		source.push("});");
 
 		this.showDemo(example, demo, source);
 	},
@@ -66,9 +101,29 @@ var PanelModule = BaseModule.extend(module, {
 	showExample3: function () {
 		var example = this.addExample("多视窗显示");
 
-		var demo = new UIGroup(this);
+		var viewports = [];
+		viewports.push({name: "view1", label: "视图 1"});
+		viewports.push({name: "view2", label: "视图 2", content: "视图2内容"});
+		viewports.push({name: "view3", label: "视图 3", content: "视图3内容"});
+		viewports.push({name: "view4", label: "视图 4"});
+
+		var demo = new UIPanel(this, {title: "标题", viewports: viewports, viewIndex: 0, content: "默认视图"});
 
 		var source = [];
+		source.push("new UIPanel(context, {");
+		source.push("    title: '标题',");
+		source.push("    content: '默认视图',");
+		source.push("    viewports: [{");
+		source.push("        name: 'view1', label: '视图 1'");
+		source.push("    }, {");
+		source.push("        name: 'view2', label: '视图 2', content: '视图2内容'");
+		source.push("    }, {");
+		source.push("        name: 'view3', label: '视图 3', content: '视图3内容'");
+		source.push("    }, {");
+		source.push("        name: 'view4', label: '视图 4'");
+		source.push("    }],");
+		source.push("    viewIndex: 0");
+		source.push("});");
 
 		this.showDemo(example, demo, source);
 	}
