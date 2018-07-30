8fe6e663c7d51e8e5178a33e1a8f53bdfa4fd00f
shicy <shicy85@163.com>
2018-07-30 23:55:54 +0800

    组件 confirm 文档修改

--- pages/components/modules/confirm.js
+++ pages/components/modules/confirm.js
@@ -35,9 +35,9 @@ var ConfirmModule = BaseModule.extend(module, {
 		var example = this.addExample("基本使用");
 
 		var demo = new UIGroup(this);
-		demo.append(new UIButton(this, {ref: "tooltip_btn1", label: "点击打开确认框"}));
+		demo.append(new UIButton(this, {ref: "confirm_btn1", label: "点击打开确认框"}));
 		// 后台构建，默认打开
-		demo.append(new UIConfirm(this, {title: "提示！！", content: "您是否确认操作？"}));
+		demo.append(new UIConfirm(this, {title: "提示！！", content: "这是一个服务端创建的确认对话框"}));
 
 		var source = [];
 		source.push("new UIConfirm(context, {title: '提示！！', content: '您是否确认操作？'});");
@@ -49,7 +49,7 @@ var ConfirmModule = BaseModule.extend(module, {
 		var example = this.addExample("自定义按钮");
 
 		var demo = new UIGroup(this);
-		demo.append(new UIButton(this, {ref: "tooltip_btn2", label: "点击打开确认框"}));
+		demo.append(new UIButton(this, {ref: "confirm_btn2", label: "点击打开确认框"}));
 
 		var source = [];
 		source.push("new UIConfirm(context, {content: '自定义“确认”和“取消”按钮', " +
